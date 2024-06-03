7f71f18337f1c0abbb0883e9c7491b1ce4e29036
jansulzberger <jansulzberger@googlemail.com>
2024-06-03 07:36:49 +0200

    Default Config

--- plugins/wordpress_image/config/config.default.php
+++ plugins/wordpress_image/config/config.default.php
@@ -2,10 +2,10 @@
 
 $wordpress_image_url='https://testwordpress.sulze.online';
 $wordpress_image_user='jansulzberger';
-$wordpress_image_key='rkb5 7rqJ J8Z9 manZ ljCf 1DTU';
+$wordpress_image_key='bla';
 
 $wordpress_image_collection_length='2';
 
 $wordpress_image_openai_url='https://api.openai.com/v1/chat/completions';
 $wordpress_image_openai_user='';
-$wordpress_image_openai_key='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';
+$wordpress_image_openai_key='bla';

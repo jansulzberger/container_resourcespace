5b3d1dad184d6e6a1600221f3fd04d23dd9130ee
jansulzberger <jansulzberger@googlemail.com>
2024-06-03 07:38:08 +0200

    Default Config

--- plugins/wordpress_image/config/config.default.php
+++ plugins/wordpress_image/config/config.default.php
@@ -6,6 +6,6 @@ $wordpress_image_key='bla';
 
 $wordpress_image_collection_length='2';
 
-$wordpress_image_openai_url='https://api.openai.com/v1/chat/completions';
+$wordpress_image_openai_url='';
 $wordpress_image_openai_user='';
 $wordpress_image_openai_key='bla';
